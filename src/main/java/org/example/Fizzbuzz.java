ae2bb4747906c94a1caf0df71730dbe7c6afe99b
Andreea <andreea.celmare@gmail.com>
2024-10-31 15:04:53 +0100

    first commit

---
+++ src/main/java/org/example/Fizzbuzz.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Fizzbuzz {
+
+    public String checkNumber(int number) {
+        if((number%3 == 0) && (number%5 == 0)){
+            return "Fizzbuzz";
+        }
+        if (number%3 == 0) {
+            return "Fizz";
+        }
+
+        if (number%5 == 0) {
+            return "Buzz";
+        }
+        if((number%3 != 0) && (number%5 != 0)){
+            return "result";
+        }
+        return null;
+    }
+}
+
---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,68 @@
+import org.example.Fizzbuzz;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FizzBuzzTest {
+    @Test
+    @DisplayName("Test si el número es divisible por 3")
+    void test_whenTheNumberIsDivisibleByThree_thenReturnFizz(){
+        //Given
+        Fizzbuzz fizzbuzz = new Fizzbuzz();
+
+
+        //When
+        String result = fizzbuzz.checkNumber(9);
+
+
+        //Then
+        assertEquals("Fizz", result);
+
+
+    }
+
+@Test
+@DisplayName("test si el numero es divisible por 5")
+    void test_whenTheNumberIsDivisibleByFive_thenReturnBuzz() {
+        //Given
+    Fizzbuzz objectOfFizzBuzz = new Fizzbuzz();
+
+        //When
+    String result = objectOfFizzBuzz.checkNumber(10);
+
+
+        //Then
+    assertEquals("Buzz", result);
+
+}
+
+@Test
+@DisplayName("test si el numero es divisible por 5 y por 3")
+    void test_whenTheNumberIsDivisibleByFiveAndThree_thenReturnFizzBuzz(){
+    //Given
+    Fizzbuzz objectOfFizzBuss = new Fizzbuzz();
+
+
+    //When
+String result = objectOfFizzBuss.checkNumber(15);
+
+    //Then
+        assertEquals("Fizzbuzz", result);
+}
+
+@Test
+@DisplayName("test si el numero no es multiple de 3 y 5")
+    void test_whenTheNumberIsNotDivisibleByFiveAndThree_thenReturnNumber(){
+    //Given
+    Fizzbuzz objectOfFizzNumber = new Fizzbuzz();
+
+
+    //When
+String result = objectOfFizzNumber.checkNumber(7);
+
+    //Then
+    assertEquals("result", result);
+}
+}
+
